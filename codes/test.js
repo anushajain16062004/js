0a60cd37de04b65f1905f55ca15d3f3320c7a934
anushajain16062004 <141549027+anushajain16062004@users.noreply.github.com>
2024-03-27 12:14:52 +0000

    conatiner config done

---
+++ codes/test.js
@@ -0,0 +1,64 @@
+//original documentation-https://ecma-international.org/publications-and-standards/standards/ecma-262/
+
+console.log("Anusha")
+//alert(3+3)- happens in browser, not nodejs
+
+
+const accountid = 1
+let accountemail = "abcd"
+accountemail = " xy"
+
+var accountpassword = "12"
+accountpassword = "54"
+
+let anusha;
+
+accountcity = "delhi"
+
+console.log(accountid)
+
+console.table([accountemail,accountid,accountpassword,anusha]);
+
+//number-2 to power 53
+
+
+//------------------------------------------------------------------------------
+
+let name ="anusha";
+let age= 19;
+let abc = true;
+
+// number 2^53
+// bigint for big numbers
+// string ""
+// boolean 
+// null - stand alone value
+// undefined - value defined nhi kari 
+// symbol - unique
+
+
+//  object 
+
+console.log(typeof age)
+//type of undefined=undefined
+//type of null=object
+
+
+//-------------------------------------------------------------------------------
+
+let score="33";
+console.log(typeof score);
+
+let valueinNumber=Number(score);
+console.log(typeof valueinNumber);
+
+//"33acb"->NaN(not a number)
+//true->1, false->0
+
+let islogged="";
+let bolleanislogged=Boolean(islogged);
+console.log(bolleanislogged);
+
+//-----------------------------------------------------------------------------------
+
+

8bf7b62fd1346c5380d6703d6fcbfd8f0c255f1d
anushajain16062004 <141549027+anushajain16062004@users.noreply.github.com>
2024-03-27 12:28:34 +0000

    1

--- codes/test.js
+++ codes/test.js
@@ -22,7 +22,7 @@ console.table([accountemail,accountid,accountpassword,anusha]);
 //number-2 to power 53
 
 
-//------------------------------------------------------------------------------
+//----------------------------------------------------------------------------------------------------------------
 
 let name ="anusha";
 let age= 19;
@@ -44,7 +44,7 @@ console.log(typeof age)
 //type of null=object
 
 
-//-------------------------------------------------------------------------------
+//----------------------------------------------------------------------------------------------------------------
 
 let score="33";
 console.log(typeof score);
@@ -59,6 +59,32 @@ let islogged="";
 let bolleanislogged=Boolean(islogged);
 console.log(bolleanislogged);
 
-//-----------------------------------------------------------------------------------
+//----------------------------------------------------------------------------------------------------------------
+//OPERATIONS
 
+let value=3;
+let negvalue=-value;
+console.log(negvalue);
+
+let str1="anusha"
+let str2="divyansh"
+
+let str3=str1 + str2;
+console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1+"2");
+// console.log("1"+2+2);//122
+// console.log(1+2+"2");//32
+// console.log((3+4)*5%3);
+
+console.log(+true)//1
+console.log(+"")//0
+
+let gameCounter=100;
+gameCounter++;
+console.log(gameCounter);
+
+
+//----------------------------------------------------------------------------------------------------------------
 
